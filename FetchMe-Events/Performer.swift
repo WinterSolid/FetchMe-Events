d501c2bd7336481d47e9b4b6900bcf2c9376d09a
WinterSolid <58896705+WinterSolid@users.noreply.github.com>
2021-09-02 14:16:20 -0500

    Project consolidation

---
+++ FetchMe-Events/Performer.swift
@@ -0,0 +1,12 @@
+//
+//  Performer.swift
+//  FetchMe-Events
+//
+//  Created by Zakee Tanksley on 9/1/21.
+//Image representation of event
+
+import Foundation
+
+struct Performer: Codable {
+    public let image: String?
+}
